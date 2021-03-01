4aaf6336324d81171a656ce00c0b7f735ff525a5
Yannick Knoll <yannknll@gmail.com>
2021-03-01 12:20:13 +0100

    Add LazyColumn for Content1

--- app/src/main/java/com/example/compose/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/example/compose/bottomsheet/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.compose.bottomsheet
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.compose.bottomsheet.ui.theme.AppTheme
@@ -30,8 +32,8 @@ class MainActivity : AppCompatActivity() {
 fun App() {
     AppTheme {
         val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
-        val scope = rememberCoroutineScope()
-        var selected by remember(calculation = { mutableStateOf(0) })
+
+        val (selected, setSelected) = remember(calculation = { mutableStateOf(0) })
 
         ModalBottomSheetLayout(sheetState = sheetState, sheetContent = {
             when (selected) {
@@ -39,37 +41,50 @@ fun App() {
                 1 -> Layout2()
             }
         }) {
+            Content(sheetState = sheetState, setSelected = setSelected)
+        }
+    }
+}
 
-            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-                Button(onClick = {
-                    selected = 0
-                    scope.launch {
-                        sheetState.show()
-                    }
-
-
-                }) {
-                    Text(text = "Show First")
-                }
-                Button(onClick = {
-                    selected = 1
-                    scope.launch {
-                        sheetState.show()
-                    }
-                }) {
-                    Text(text = "Show Second")
-                }
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun Content(sheetState: ModalBottomSheetState, setSelected: (Int) -> Unit) {
+    val scope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            setSelected(0)
+            scope.launch {
+                sheetState.show()
             }
+        }) {
+            Text(text = "Show First")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            setSelected(1)
+            scope.launch {
+                sheetState.show()
+            }
+        }) {
+            Text(text = "Show Second")
         }
     }
 }
 
 @Composable
 private fun Layout2() {
-    Text(
-        modifier = Modifier.padding(16.dp),
-        text = "BottomSheetLayout 2"
-    )
+    LazyColumn(Modifier.fillMaxWidth()) {
+        items(50) {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "BottomSheetLayout:$it"
+            )
+        }
+    }
 }
 
 @Composable
